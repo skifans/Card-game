9e9fcc038f6f5b2b898e11328e36bb771ad1a80f
Alex <skifans@users.noreply.github.com>
2016-03-19 15:42:50 +0000

    Create equipment list.py

---
+++ cards/equipment list.py
@@ -0,0 +1,112 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      callum
+#
+# Created:     24,02,2016
+# Copyright:   (c) callum 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# Order for Arrays: Type, Attack,defence, Duribility, mana cost, name, effect or not
+
+
+#attack left
+#durability right
+#mana midel
+
+
+#Smith:
+
+Broken_sword = [0,1,4,1,"Broken sword",0]
+Glass_blade = [0,5,1,3,"Glass Blade",0]
+Smithing_hammer = [0,1,5,4, "Smithing Hammer",1]
+Large_hammer = [0,6,4,6, "Large Hammer",0]
+
+Gloves = [1,2,2,2,"Gloves",0]
+Smithing_hands = [1,2,3,3,"Smith's Hands",1]
+Forge_gloves = [1,1,4,6,"Forge Gloves",1]
+
+Leg_brace = [2,2,2,2,"Leg brace",0]
+Smiths_leg_guard = [2,3,4,3,"Smith's Leg guards",0]
+Peg_leg = [2,1,4,4,"Peg Leg",1]
+
+Face_mask =[3,2,2,2,"Face Mask",0]
+Goggles = [3,0,5,3,"Goggles",1]
+Forge_guard = [3,4,5,4,"Forge Guard",0]
+
+Smiths_apron = [4,2,2,2,"Smith's Apron",0]
+Anvil = [4,3,3,"Anvil",1]
+Forged_armour = [4,5,5,5,"Forged Armour",0]
+
+
+#Paladin
+
+Hammer_of_dawn = [0,2,3,1,"Hammer of Dawn",0]
+Lance = [0,2,5,3,"Lance",0]
+Holy_blade = [0,2,6,4,"Holy Blade",1]
+Blessed_lance = [0,7,4,6,"Blessed Lance",1]
+
+Steel_gauntlets = [1,1,3,2,"steel Gauntlets",0]
+Valkyrie_gloves =  [1,3,3,3,"Valkyrie Gloves",1]
+GigaSteel_gauntlets = [1,4,6,5,"GigaSteel Gauntlets",0]
+
+Steel_greaves = [2,3,2,2, "Steel Greaves",0]
+Valkyrie_sandles = [2,3,3,3,"Valkyrie Sandles",0]
+GigaSteel_greaves = [2,4,6,5,"GigaSteel Greaves",0]
+
+Steel_helm= [3,3,2,2,"Steel Helm",0]
+Valkyrie_helm = [3,3,3,3,"Valkyrie helm",1]
+GigaSteel_helm = [3,4,6,5, "GigaSteel Helm",1]
+
+Steel_mail = [4,3,2,2,"Steel Mail",0]
+Valkyrie_armour = [4,4,3,4,"Valkyrie Armour",0]
+GigaSteel_platemail = [4,4,6,5, "Gigasteel Plate-mail",0]
+
+#Man at arms
+
+Mace = [0,5,1,2, "Mace",0]
+Heavy_hammer = [0,6,2,4, "Heavy hammer",0]
+Claymore = [0,8,2,6,"Claymore",0]
+Reckless_sword = [0,10,1,8, "Reckless_sword",0]
+
+Bandages =[1,3,1,2,"Bandages",0]
+Hand_guard = [1,2,4,4,"Hand Guard"]
+Spiked_shield = [1,4,4,6,"Spiked Shield",1]
+
+Boots = [2,3,1,2,"Boots",0]
+Kilt = [2,2,4,4,"Kilt",0]
+Swift_boots = [2,4,3,6,"Swift Boots",1]
+
+Eye_patch = [3,3,1,2,"Eye patch",0]
+Steel_mask = [3,2,4,4, "Steel Mask",0]
+Conqurers_helm = [3,4,5,6,"Conqurers Helm",0]
+
+Blood_stained_wraps = [4,3,1,2,"Blood Stained wraps",0]
+Woad = [4,2,4,4,"Woad",0]
+Conqurers_armour = [4,4,5,6,"Conqurers armour",0]
+
+#General
+
+Standard_sword=[0,2,3,2,"Standard sword",0]
+Buckler = [0,0,4,2,"Buckler",0]
+
+
+
+Leather_gloves	=[1,1,2,1,"Leather gloves",0]
+Iron_gauntlents	=[1,3,5,4,"Iron gauntlents",0]
+
+
+Leather_greeves = [2,3,3,3,"Leather greeves",0]
+Iron_greeves = [2,3,5,4, "Iron greeves",0]
+
+
+Heros_cap = [3,1,3,1,"Hero's cap",0]
+Heros_helm = [3,3,3,3,"Hero's helm",0]
+
+
+Iron_armour = [4,3,5,4,"Iron armour",0]
+Steel_armour = [4,3,6,5,"Steel armour",0]
+
+
